cca5e57ba5ed22270984f3084b20e30fa6e68098
sasayabaku <sasayabaku@yahoo.co.jp>
2021-01-03 21:10:48 +0900

    add landing illust and fix setting grid parameter

--- components/intro/index.vue
+++ components/intro/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="introduce">
         <v-row>
-            <v-col cols="12" sm="12" md="7" align="center">
+            <v-col cols="12" sm="12" md="6" lg="6" align="center">
                 <div class="text">
                     <div class="accent">
                         About Me.
@@ -11,10 +11,11 @@
                     </div>
                 </div>
             </v-col>
-            <v-col cols="12" sm="12" md="5">
+            <v-col cols="12" sm="12" md="6" lg="6" align="center">
                 <div class="image">
-
+                    <img src='developer_illust.svg'/>
                 </div>
+                <div class="copyright">illustrate on <a href="https://undraw.co">unDraw</a></div>
             </v-col>
         </v-row>
     </div>
@@ -30,13 +31,19 @@ export default {
     .introduce {
         // background: linear-gradient(to right, rgba(0, 0, 0, 0.6) 0%, rgba(30,30,30,0.6) 20%, rgba(252, 252, 252, 0.6) 100%), url('/coding.jpg');
         // background-size: cover;
+        .image {
+            height: 10rem;
+            img {
+                height: 100%;
+            }
+        }
     }
 
     .text {
 
         display: block;
-        width: 70%;
-        padding: 0 3rem;
+        width: 90%;
+        margin: 0 10rem;
 
         .accent {
             text-align: left;
@@ -50,6 +57,18 @@ export default {
             text-align: left;
             font-size: 1.2rem;
             color: #626a7f;
+        }
+    }
+
+    .copyright {
+        font-size: 0.8rem;
+        margin-top: 0.5rem;
+        padding-left: 2rem;
+        color: #cecece;
+
+        a {
+            text-decoration-line: underline;
+            color: #cecece;
         }
     }
 </style>
